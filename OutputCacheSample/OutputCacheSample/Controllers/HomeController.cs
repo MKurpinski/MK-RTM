2c9a7ed123695185a7481297e0f11c5288e29c21
mkurpinski <michal.kurpinski96@gmail.com>
2018-12-23 14:33:27 +0100

    Output cache

---
+++ OutputCacheSample/OutputCacheSample/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace OutputCacheSample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [OutputCache(Duration = 10, Location = OutputCacheLocation.Any, VaryByParam = "none")]
+        public ActionResult Index()
+        {
+            ViewBag.CurrentDate = DateTime.UtcNow;
+            return View();
+        }
+
+        [OutputCache(Duration = 10, Location = OutputCacheLocation.Client, VaryByParam = "none")]
+        public ActionResult About()
+        {
+            ViewBag.UserId = Guid.NewGuid().ToString("d");
+            return View();
+        }
+
+        [OutputCache(CacheProfile = "CacheForTenSeconds")]
+        public ActionResult Contact()
+        {
+            ViewBag.CurrentDate = DateTime.UtcNow;
+            return View("Index");
+        }
+
+        [OutputCache(Duration = 10, Location = OutputCacheLocation.Any, VaryByParam = "id")]
+        public ActionResult Details(int id)
+        {
+            ViewBag.CurrentDate = DateTime.UtcNow;
+            ViewBag.IdParam = id;
+            return View();
+        }
+    }
+}
